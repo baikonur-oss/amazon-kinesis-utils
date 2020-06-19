1d388a90150b2985c4ebd3fff3ad08bfd16b8afb
Torgayev Tamirlan <torgayev_tamirlan_xb@cyberagent.co.jp>
2020-06-19 18:53:26 +0900

    Add test case for parse_records: plaintext multiple

--- tests/test_kinesis.py
+++ tests/test_kinesis.py
@@ -4,6 +4,34 @@ import unittest
 from amazon_kinesis_utils import kinesis
 
 
+def generate_sample_kinesis_records(data: list) -> list:
+    ret = []
+
+    for d in data:
+        data_blob = d.encode()
+        data_base64 = base64.b64encode(data_blob)
+        ret.append(
+            {
+                "kinesis": {
+                    "kinesisSchemaVersion": "1.0",
+                    "partitionKey": "0",
+                    "sequenceNumber": "00000000000000000000000000000000000000000000000",
+                    "data": data_base64,
+                    "approximateArrivalTimestamp": 1592558220.000,
+                },
+                "eventSource": "aws:kinesis",
+                "eventVersion": "1.0",
+                "eventID": "shardId-000000000000:00000000000000000000000000000000000000000000000",
+                "eventName": "aws:kinesis:record",
+                "invokeIdentityArn": "arn:aws:iam::000000000000:role/service-role/foo",
+                "awsRegion": "ap-northeast-1",
+                "eventSourceARN": "arn:aws:kinesis:ap-northeast-1:000000000000:stream/bar",
+            }
+        )
+
+    return ret
+
+
 class KinesisTests(unittest.TestCase):
     def test_create_record(self):
         data = "test_data"
@@ -34,32 +62,23 @@ class KinesisTests(unittest.TestCase):
             self.assertTrue(len(record["PartitionKey"]) <= 256)
 
     def test_parse_records_plaintext(self):
-        data = "plain text record sample"
-        data_blob = data.encode()
-        data_base64 = base64.b64encode(data_blob)
+        data = ["plain text record sample"]
 
-        event = {
-            "Records": [
-                {
-                    "kinesis": {
-                        "kinesisSchemaVersion": "1.0",
-                        "partitionKey": "0",
-                        "sequenceNumber": "00000000000000000000000000000000000000000000000",
-                        "data": data_base64,
-                        "approximateArrivalTimestamp": 1592558220.000,
-                    },
-                    "eventSource": "aws:kinesis",
-                    "eventVersion": "1.0",
-                    "eventID": "shardId-000000000000:00000000000000000000000000000000000000000000000",
-                    "eventName": "aws:kinesis:record",
-                    "invokeIdentityArn": "arn:aws:iam::000000000000:role/service-role/foo",
-                    "awsRegion": "ap-northeast-1",
-                    "eventSourceARN": "arn:aws:kinesis:ap-northeast-1:000000000000:stream/bar",
-                }
-            ]
-        }
+        event = {"Records": generate_sample_kinesis_records(data)}
 
         records = [x for x in kinesis.parse_records(event["Records"])]
 
-        self.assertEqual(len(records), 1)
-        self.assertEqual(records[0], data)
+        self.assertEqual(len(records), len(data))
+        for i, r in enumerate(records):
+            self.assertEqual(r, data[i])
+
+    def test_parse_records_plaintext_multiple(self):
+        data = [f"test-data-{x}" for x in range(10)]
+
+        event = {"Records": generate_sample_kinesis_records(data)}
+
+        records = [x for x in kinesis.parse_records(event["Records"])]
+
+        self.assertEqual(len(records), len(data))
+        for i, r in enumerate(records):
+            self.assertEqual(r, data[i])
