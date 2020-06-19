e4e70cba016cc996cb856635763defeaece53139
Torgayev Tamirlan <torgayev_tamirlan_xb@cyberagent.co.jp>
2020-06-19 18:27:11 +0900

    Add test case for parse_records: plaintext

--- tests/test_kinesis.py
+++ tests/test_kinesis.py
@@ -1,3 +1,4 @@
+import base64
 import unittest
 
 from amazon_kinesis_utils import kinesis
@@ -31,3 +32,34 @@ class KinesisTests(unittest.TestCase):
 
             self.assertTrue(type(record["PartitionKey"]) == str)
             self.assertTrue(len(record["PartitionKey"]) <= 256)
+
+    def test_parse_records_plaintext(self):
+        data = "plain text record sample"
+        data_blob = data.encode()
+        data_base64 = base64.b64encode(data_blob)
+
+        event = {
+            "Records": [
+                {
+                    "kinesis": {
+                        "kinesisSchemaVersion": "1.0",
+                        "partitionKey": "0",
+                        "sequenceNumber": "00000000000000000000000000000000000000000000000",
+                        "data": data_base64,
+                        "approximateArrivalTimestamp": 1592558220.000,
+                    },
+                    "eventSource": "aws:kinesis",
+                    "eventVersion": "1.0",
+                    "eventID": "shardId-000000000000:00000000000000000000000000000000000000000000000",
+                    "eventName": "aws:kinesis:record",
+                    "invokeIdentityArn": "arn:aws:iam::000000000000:role/service-role/foo",
+                    "awsRegion": "ap-northeast-1",
+                    "eventSourceARN": "arn:aws:kinesis:ap-northeast-1:000000000000:stream/bar",
+                }
+            ]
+        }
+
+        records = [x for x in kinesis.parse_records(event["Records"])]
+
+        self.assertEqual(len(records), 1)
+        self.assertEqual(records[0], data)
