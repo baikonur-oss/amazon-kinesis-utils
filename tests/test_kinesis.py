653d6d8dbb326ffd32ca7a7839c6298e059db615
Torgayev Tamirlan <torgayev_tamirlan_xb@cyberagent.co.jp>
2020-06-22 20:36:57 +0900

    Add more CWL subscription filters test cases

--- tests/test_kinesis.py
+++ tests/test_kinesis.py
@@ -94,7 +94,7 @@ class KinesisTests(unittest.TestCase):
         for i, r in enumerate(records):
             self.assertEqual(r, data[i])
 
-    def test_parse_records_json_multi(self):
+    def test_parse_records_json_multiple(self):
         json_data = [{"a": 1}, {"b": 2}, {"c": 3}]
 
         data = [json.dumps(x) for x in json_data]
@@ -117,23 +117,60 @@ class KinesisTests(unittest.TestCase):
 
         self.assertEqual(len(records), 0)
 
-    def test_parse_records_gzipped_json_cwl(self):
+    def test_parse_records_cwl_health_check(self):
         # raw sample data from CloudWatch Logs Subscription Filters
         # containing only a health check message:
         # "CWL CONTROL MESSAGE: Checking health of destination Kinesis stream."
 
         data = [
-            "H4sIAAAAAAAAADWOwQqCQBRFf2WYdURGFrkLsRZZQgYtQmLSlz7SGZk3JhH+e6PW8nAv954Pr4BI5HB+18A97kfH8ykKb4cgjje7gE"
-            "+4aiXoPilVk7XCpEWocrJBqfKdVk1ts5Fio0FUI1Jzp1RjbVDJLZYGNHHvmgy94AXS9PjhmI11g1bDiMqOOe567i4XznK2ctzJX68X"
-            "uITsp8d+eh7zC0ifKHNWgChNwdSDZXYJpeif2R4lEBKjQW3Ku6T7ApsNvwTyAAAA"
+            "H4sIAAAAAAAAADWOwQqCQBRFf2WYdURGFrkLsRZZQgYtQmLSlz7SGZk3JhH+e6PW8nAv954Pr4BI5HB+18A97kfH8ykKb4cgjje7gE+4ai"
+            "XoPilVk7XCpEWocrJBqfKdVk1ts5Fio0FUI1Jzp1RjbVDJLZYGNHHvmgy94AXS9PjhmI11g1bDiMqOOe567i4XznK2ctzJX68XuITsp8d+"
+            "eh7zC0ifKHNWgChNwdSDZXYJpeif2R4lEBKjQW3Ku6T7ApsNvwTyAAAA"
         ]
 
         event = {"Records": generate_sample_kinesis_records(data, encode=False)}
 
-        import logging
+        records = [x for x in kinesis.parse_records(event["Records"])]
 
-        logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+        # control messages should be ignored
+        self.assertEqual(len(records), 0)
+
+    def test_parse_records_cwl_payload(self):
+        # raw sample data from CloudWatch Logs Subscription Filters
+        # containing a single DATA_MESSAGE with a plain text payload:
+        # "hello"
+
+        data = [
+            "H4sIANWN8F4C/02PzQrCMBCE3yVnD83m31vB6slTexORqosG2qYkUZHiu7taBPf47czszsR6TKm9YPMckS3ZqmzKw7aq63JTsQULjwEj4e"
+            "JvCHfhsonhNtImY8ozqXPEtp/FRNLtmE7Rj9mHYe27jDGx5W7W77+G6o5D/sCJ+TP5hFJcCQsSLBgHvACtFRRSG2OhMMJyp6RyVmrNrQEt"
+            "nDYg4HMse6qR254+4sqB0pIiJBeLXz2Kv2LXBfbav941C5T39AAAAA=="
+        ]
+
+        event = {"Records": generate_sample_kinesis_records(data, encode=False)}
 
         records = [x for x in kinesis.parse_records(event["Records"])]
 
-        self.assertEqual(len(records), 0)
+        self.assertEqual(len(records), 1)
+
+        self.assertEqual(records[0], "hello")
+
+    def test_parse_records_cwl_health_check_payload(self):
+        # raw sample data from CloudWatch Logs Subscription Filters
+        # containing a single health check and two DATA_MESSAGE payloads
+        data = [
+            "H4sIAPSW8F4C/6WPzWrDQAyE30VnH7za/9wMdXLKybmVENxWpAu21+xuUkrwu1eJKRRKT9Xxk2ZGc4ORcu7PdPicCTbw1Bya077tumbXQg"
+            "XxY6LEuP4xjId43qV4mXlTKJeVdCVRP67HTPLlJb+mMJcQp20YCqUMm+f1/vgQtFeayh3eILyxTmottHSo0KH1KGo0RmOtjLUOayud8Fpp"
+            "75Qxwlk00huLEu9hJXCN0o/8kdAetVFsoYSsvuux/TsNQxSwVP+LE3/HqV9xCMtx+QJguUFbZAEAAA==",
+            "H4sIAPCW8F4C/zWOwQqCQBRFf2WYdURGFrkLsRZZQgYtQmLSlz7SGZk3JhH+e6PW8nAv954Pr4BI5HB+18A97kfH8ykKb4cgjje7gE+4ai"
+            "XoPilVk7XCpEWocrJBqfKdVk1ts5Fio0FUI1Jzp1RjbVDJLZYGNHHvmgy94AXS9PjhmI11g1bDiMqOOe567i4XznK2ctzJX68XuITsp8d+"
+            "eh7zC0ifKHNWgChNwdSDZXYJpeif2R4lEBKjQW3Ku6T7ApsNvwTyAAAA",
+        ]
+
+        event = {"Records": generate_sample_kinesis_records(data, encode=False)}
+
+        records = [x for x in kinesis.parse_records(event["Records"])]
+
+        self.assertEqual(len(records), 2)
+
+        self.assertEqual(records[0], "hello1")
+        self.assertEqual(records[1], "hello2")
